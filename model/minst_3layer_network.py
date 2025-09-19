4e1cafa33216913d2a668cadc10fd059a10ee85c
gisu <iron8233@gmail.com>
2025-09-19 19:19:09 +0900

    [feat} #4 - 3층 신경망 구현

--- model/minst_3layer_network.py
+++ model/minst_3layer_network.py
@@ -1,9 +1,58 @@
 import numpy as np
+import sys, os
+import pickle
+sys.path.append(os.pardir)
+from dataset.mnist import load_minst
+from trainer.activation import sigmoid, softmax
+from trainer.loss import cross_entropy_error
+from trainer.optimizer import numerical_gradient, gradient_descent
 
-# regression task -> identity function, classification -> softmax function
-def softmax(x):
-    exp_x = np.exp(x)
-    sum_exp_x = np.sum(exp_x)
-    y = exp_x / sum_exp_x
+class ThreeLayerNet:
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=1e-2):
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, hidden_size)
+        self.params['b2'] = np.zeros(hidden_size)
+        self.params['W3'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b3'] = np.zeros(output_size)
+        
+    def predict(self, x):
+        W1, W2, W3 = self.params['W1'], self.params['W2'], self.params['W3']
+        b1, b2, b3 = self.params['b1'], self.params['b2'], self.params['b3']
+        
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        z2 = sigmoid(a2)
+        a3 = np.dot(z2, W3) + b3
+        z3 = sigmoid(a3)
+        y = softmax(z3)
+        
+        return y
     
-    return y
+    def loss(self, x, t):
+        y = self.predict(x)
+        
+        return cross_entropy_error(y, t)
+    
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis= 1)
+        t = np.argmax(t, axis= 1)
+        
+        accuracy = np.sum(y==t) / float(len(x.shape[0]))
+        return accuracy
+    
+    def get_gradient(self, x, t):
+        loss_W = lambda W: self.loss(x, t)
+        
+        grads = {}
+        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+        grads['W3'] = numerical_gradient(loss_W, self.params['W3'])
+        grads['b3'] = numerical_gradient(loss_W, self.params['b3'])
+        
+        return grads
