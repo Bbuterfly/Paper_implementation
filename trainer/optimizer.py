[fix] #4 - numerical_gradient 함수를 다차원 인덱스를 사용하도록 수정

trainer/optimizer.py
@@ -1,11 +1,15 @@
 import numpy as np
 
 # 중앙 차분식 수치미분법 -> f'(x) = f(x+h) - f(x-h) / 2h
+# 어차피 backward를 통해서 grad를 계산하기 때문에 사용하지 않음
 def numerical_gradient(f, x):
     h = 1e-4
     grad = np.zeros_like(x)
     
-    for idx in range(x.size):
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    
+    while not it.finished:
+        idx = it.multi_index
         tmp_val = x[idx]
 
         x[idx] = tmp_val + h
@@ -16,6 +20,8 @@ def numerical_gradient(f, x):
         
         grad[idx] = (fxh1 - fxh2) / 2*h
         x[idx] = tmp_val
+        
+        it.iternext()
         
     return grad
     