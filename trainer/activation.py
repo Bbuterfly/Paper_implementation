58922ab852507f2d703e8a36bc61a8af23bedf5a
gisu <iron8233@gmail.com>
2025-10-04 22:33:14 +0900

    [feat] #4 - ReLU 구현

--- trainer/activation.py
+++ trainer/activation.py
@@ -1,5 +1,21 @@
 import numpy as np
 
+class Relu:
+    def __init__(self):
+        self.mask = None
+    
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+        
+        return out
+    
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+        
+        return dx
 # regression task -> identity function, classification -> softmax function
 def softmax(x):
     c = np.max(x) # 오버플로우 방지, 일반적으로 입력 신호의 최댓값 사용
