ff362365c428dbfe5d27186c50480ecb97641ebf
gisu <iron8233@gmail.com>
2025-10-09 23:22:49 +0900

    [feat] #4 - 3층 신경망 코드 변경 및 backward 연산 구현

--- model/minst_3layer_network.py
+++ model/minst_3layer_network.py
@@ -1,44 +1,47 @@
 import numpy as np
 import sys, os
-import pickle
 sys.path.append(os.pardir)
-from dataset.mnist import load_mnist
-from trainer.activation import sigmoid, softmax
-from trainer.loss import cross_entropy_error
-from trainer.optimizer import numerical_gradient, gradient_descent
+from trainer.optimizer import numerical_gradient
+from collections import OrderedDict
+from model.layers import Affine, Relu, SoftmaxWithLoss
 
 class ThreeLayerNet:
     def __init__(self, input_size, hidden_size, output_size, weight_init_std=1e-2):
+        # 가중치 초기화
         self.params = {}
         self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
         self.params['b1'] = np.zeros(hidden_size)
         self.params['W2'] = weight_init_std * np.random.randn(hidden_size, hidden_size)
         self.params['b2'] = np.zeros(hidden_size)
-        self.params['W3'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['W3'] = weight_init_std * np.random.randn(hidden_size, output_size)        
         self.params['b3'] = np.zeros(output_size)
         
+        # Layer 생성
+        self.layers = OrderedDict()
+        self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
+        self.layers['Relu1'] = Relu()
+        self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
+        self.layers['Relu2'] = Relu()
+        self.layers['Affine3'] = Affine(self.params['W3'], self.params['b3'])
+        
+        self.lastLayer = SoftmaxWithLoss()
+        
     def predict(self, x):
-        W1, W2, W3 = self.params['W1'], self.params['W2'], self.params['W3']
-        b1, b2, b3 = self.params['b1'], self.params['b2'], self.params['b3']
-        
-        a1 = np.dot(x, W1) + b1
-        z1 = sigmoid(a1)
-        a2 = np.dot(z1, W2) + b2
-        z2 = sigmoid(a2)
-        a3 = np.dot(z2, W3) + b3
-        y = softmax(a3)
-        
-        return y
+        for layer in self.layers.values():
+            x = layer.forward(x)
+            
+        return x
     
     def loss(self, x, t):
         y = self.predict(x)
         
-        return cross_entropy_error(y, t)
+        return self.lastLayer.forward(y, t)
     
     def accuracy(self, x, t):
         y = self.predict(x)
         y = np.argmax(y, axis= 1)
-        t = np.argmax(t, axis= 1)
+        if t.dim != 1:
+            t = np.argmax(t, axis= 1)
         
         accuracy = np.sum(y==t) / float(x.shape[0])
         return accuracy
@@ -53,5 +56,27 @@ class ThreeLayerNet:
         grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
         grads['W3'] = numerical_gradient(loss_W, self.params['W3'])
         grads['b3'] = numerical_gradient(loss_W, self.params['b3'])
+        
+        return grads
+    
+    def gradient(self, x, t):
+        self.loss(x, t)
+        
+        # 역전파
+        dout = 1
+        dout = self.lastLayer.backward(dout)
+        
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+            
+        grads = {}
+        grads['W1'] = self.layers['Affine1'].dW
+        grads['b1'] = self.layers['Affine1'].db
+        grads['W2'] = self.layers['Affine2'].dW
+        grads['b2'] = self.layers['Affine2'].db
+        grads['W3'] = self.layers['Affine3'].dW
+        grads['b3'] = self.layers['Affine3'].db
         
         return grads
